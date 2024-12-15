400464a3356cb3e7582e1097ba88613426e83f81
Artyom Belousov <flygrounder@yandex.ru>
2024-12-15 16:39:57 +0300

    Make wrapper for yazi

--- modules/cli.nix
+++ modules/cli.nix
@@ -59,7 +59,17 @@
         };
         fish = {
           enable = true;
-          functions = { fish_greeting = ""; };
+          functions = {
+            fish_greeting = "";
+            y = ''
+              set tmp (mktemp -t "yazi-cwd.XXXXXX")
+              yazi $argv --cwd-file="$tmp"
+              if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+                builtin cd -- "$cwd"
+              end
+              rm -f -- "$tmp"
+            '';
+          };
           shellAliases = {
             lg = "lazygit";
             hf =
